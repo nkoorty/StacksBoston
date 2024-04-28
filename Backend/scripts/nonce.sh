e150f3ad65bc481ded54f460d6ce8b4f25388e22
Adesh Dooraree <101601277+ADoor22@users.noreply.github.com>
2024-04-28 11:44:35 -0400

    nonce check

--- Backend/scripts/nonce.sh
+++ Backend/scripts/nonce.sh
@@ -1,4 +1,4 @@
-response=$(curl -L 'http://localhost:3999/extended/v1/address/ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM/nonces' -H 'Accept: application/json')
+response=$(curl -L 'http://localhost:3999/extended/v1/address/ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5/nonces' -H 'Accept: application/json')
 nonce=$(echo "$response" | jq -r '.possible_next_nonce')
 echo $nonce
 
